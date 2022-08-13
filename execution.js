c8b9acdae29c0376909eb0762822f50159b59b27
SaiPraveen07 <107098201+SaiPraveen07@users.noreply.github.com>
2022-08-13 22:14:42 +0530

    nodejs module

---
+++ execution.js
@@ -0,0 +1,7 @@
+var cal = require('./cal')
+var x = 23 ;
+var y = 10 ;
+console.log("Add : " + cal.add(x,y));
+console.log("Sub : " + cal.sub(x,y));
+console.log("Mul : " + cal.mul(x,y));
+console.log("Div : " + cal.div(x,y));
